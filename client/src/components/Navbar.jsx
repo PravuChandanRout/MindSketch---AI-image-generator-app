4f87d8a2fb88ac9f138625e4b840b3f4808916f9
Pravu chandan Rout <140379711+Chandanrout1@users.noreply.github.com>
2024-12-11 17:47:14 +0530

    login, sign up functionality added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,10 @@
 import React, { useContext } from "react";
 import { assets } from "../assets/assets.js";
 import { Link, useNavigate } from "react-router-dom";
-import { AppContext } from "../context/appContext.jsx";
+import { AppContext } from "../context/AppContext.jsx";
 
 const Navbar = () => {
-  
-  const {user} = useContext(AppContext)
+  const { user, setShowLogin } = useContext(AppContext);
   const navigate = useNavigate();
 
   return (
@@ -18,18 +17,25 @@ const Navbar = () => {
         {user ? (
           // users details if logged in
           <div className="flex items-center gap-2 sm:gap-3">
-            <button onClick={()=> navigate("/buy")} className="flex items-center gap-2 bg-blue-100 px-4 sm:px-6 py-1.5 sm:py-3 rounded-full hover:scale-105 transition-all duration-700">
+            <button
+              onClick={() => navigate("/buy")}
+              className="flex items-center gap-2 bg-blue-100 px-4 sm:px-6 py-1.5 sm:py-3 rounded-full hover:scale-105 transition-all duration-700"
+            >
               <img className="w-5" src={assets.credit_star} alt="" />
-              <p className="text-xs sm:text-sm font-medium text-gray-700">Credit left : 50</p>
+              <p className="text-xs sm:text-sm font-medium text-gray-700">
+                Credit left : 50
+              </p>
             </button>
             <p className="text-gray-700 max-sm:hidden pl-4">Hi, Chandan</p>
             <div className="relative group">
-              <img src={assets.profile_icon} className="w-10 drop-shadow" alt="" />
+              <img
+                src={assets.profile_icon}
+                className="w-10 drop-shadow"
+                alt=""
+              />
               <div className="absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12">
                 <ul className="list-none m-0 p-2 bg-white rounded-md border text-sm">
-                  <li className="py-1 px-2 pr-10 cursor-pointer">
-                    Logout
-                  </li>
+                  <li className="py-1 px-2 pr-10 cursor-pointer">Logout</li>
                 </ul>
               </div>
             </div>
@@ -40,7 +46,7 @@ const Navbar = () => {
             <p onClick={() => navigate("/buy")} className="cursor-pointer">
               Pricing
             </p>
-            <button className="bg-blue-600 hover:bg-blue-800 text-white px-6 py-2 sm:px-10 text-sm rounded-md">
+            <button onClick={()=> setShowLogin(true)} className="bg-blue-600 hover:bg-blue-800 text-white px-6 py-2 sm:px-10 text-sm rounded-md">
               Login
             </button>
           </div>
