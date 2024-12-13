0c6fdfc2e765ded6d68c25d7497f631191f408e5
Pravu chandan Rout <140379711+Chandanrout1@users.noreply.github.com>
2024-12-13 13:31:33 +0530

    setting up express server

---
+++ server/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => res.send(`server is running on port ${PORT}`));
+
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
