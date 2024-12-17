0a97779210f2aab55c81fcde11738e9e6bb40eaa
Pravu chandan Rout <140379711+Chandanrout1@users.noreply.github.com>
2024-12-17 12:30:01 +0530

    transaction data model created

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,6 +1,7 @@
 import userModel from "../models/userModel.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import razorpay from "razorpay";
 
 // sign up user api
 const registerUser = async (req, res) => {
@@ -84,6 +85,72 @@ const usercredits = async (req, res) => {
       credits: user.creditBalance,
       user: { name: user.name },
     });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+// razorpay payment instance
+
+const razorpayInstance = new razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
+
+const paymentRazorpay = async (req, res) => {
+  try {
+    const { userId, planId } = req.body;
+
+    const userData = await userModel.findById(userId);
+
+    if (!userId || !planId) {
+      return res.json({
+        success: false,
+        message: "Missing details",
+      });
+    }
+
+    let credits, plan, amount, date;
+
+    switch (planId) {
+      case "Basic":
+        plan = "Basic";
+        credits = 100;
+        amount = 10;
+        break;
+
+      case "Advanced":
+        plan = "Advanced";
+        credits = 500;
+        amount = 50;
+        break;
+
+      case "Business":
+        plan = "Business";
+        credits = 5000;
+        amount = 250;
+        break;
+
+      default:
+        return res.json({
+          success: false,
+          message: "Plan does not exist",
+        });
+    }
+
+    date = Date.now();
+
+    const transactionData = {
+      userId,
+      plan,
+      credits,
+      amount,
+      date,
+    };
   } catch (error) {
     console.log(error);
     res.json({
